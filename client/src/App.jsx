1e36904ee8b51246836c4f617a3455cd0a79e715
Sneha Kushwaha <snehakushwaha7394@gmail.com>
2025-06-14 15:02:08 +0530

    Add imports in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-dom'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
+import MyBookings from './pages/MyBookings'
+import Favorite from './pages/Favorite'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/movies' element={<Movies/>}/>
+        <Route path='/movies/:id' element={<MovieDetails/>}/>
+        <Route path='/movies/:id/:date' element={<SeatLayout/>}/>
+        <Route path='/my-bookings' element={<MyBookings/>}/>
+        <Route path='/favorite' element={<Favorite/>}/>
+      </Routes>
+    </>
   )
 }
 
