4ef50a4464ab45ac30f7d7999ac6289e7b67be43
Sneha Kushwaha <snehakushwaha7394@gmail.com>
2025-06-15 11:57:46 +0530

    Added BlurCircle at right corner

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -1,6 +1,7 @@
 import { ArrowRight } from 'lucide-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import BlurCircle from './BlurCircle'
 
 const FeaturedSection = () => {
 
@@ -10,6 +11,7 @@ const FeaturedSection = () => {
    <div className='px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden'> 
 
     <div className='relative flex items-center justify-between pt-20 pb-10'>
+        <BlurCircle top='0' right='-80px'/>
         <p className='text-gray-300 font-medium text-lg'>Now Showing</p>
         <button onClick={()=> navigate('/movies')} className='group flex items-center gap-2 text-sm text-gray-300'>
             View All
@@ -19,7 +21,9 @@ const FeaturedSection = () => {
 
     <div></div>
 
-    <div></div>
+    <div className='flex justify-center mt-20'>
+        <button onClick={()=>  {navigate('/movies'); scrollTo(0,0)}} className='px-10 py-3 text-sm  bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer'>Show more</button>
+    </div>
 
    </div>
   )
