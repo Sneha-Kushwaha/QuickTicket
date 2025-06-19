b12ca1d7264f06db6e128d8cbaa651998cf32628
Sneha Kushwaha <snehakushwaha7394@gmail.com>
2025-06-19 12:42:31 +0530

    now time is selected

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -36,7 +36,7 @@ const SeatLayout = () => {
       <p className='text-lg font-semibold px-6'>Available Timings</p>
       <div className='mt-5 space-y-1'>
         {show.dateTime[date].map((item)=>(
-          <div className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition ${selectedTime?.time === item.time ? "bg-primary text-white" : "hover:bg-primary/20"}`}>
+          <div key={item.time} onClick={()=> setSelectedTime(item)} className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition ${selectedTime?.time === item.time ? "bg-primary text-white" : "hover:bg-primary/20"}`}>
             <ClockIcon className="w-4 h-4"/>
             <p className='text-sm'>{isoTimeFormat(item.time)}</p>
           </div>
@@ -45,7 +45,9 @@ const SeatLayout = () => {
       </div>
 
        {/* Seats Layout */}
-       <div></div>
+       <div>
+        
+       </div>
     </div>
   ) : (
     <Loading />
