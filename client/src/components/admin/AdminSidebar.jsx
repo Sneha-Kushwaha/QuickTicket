dc3ce7f6eb08e77ae551881ceea83ae4025ba417
Sneha Kushwaha <snehakushwaha7394@gmail.com>
2025-06-20 12:57:54 +0530

    Added navbar in admin page

--- client/src/components/admin/AdminSidebar.jsx
+++ client/src/components/admin/AdminSidebar.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const Dashboard = () => {
+export const AdminSidebar = () => {
   return (
     <div>
-        
+        <h1>Sidebar</h1>
     </div>
   )
 }
-
-export default Dashboard
