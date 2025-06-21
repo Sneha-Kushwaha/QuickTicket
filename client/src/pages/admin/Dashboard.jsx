cef27de02464f60c6b7c057673aeb6a6c8d1e57b
Sneha Kushwaha <snehakushwaha7394@gmail.com>
2025-06-21 12:34:01 +0530

    added active shows in dashboard

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,8 +1,9 @@
-import { ChartLineIcon, CircleDollarSignIcon, PlayCircleIcon, UsersIcon } from 'lucide-react';
+import { ChartLineIcon, CircleDollarSignIcon, PlayCircleIcon, StarIcon, UsersIcon } from 'lucide-react';
 import React, { useEffect, useState } from 'react'
 import { dummyDashboardData } from '../../assets/assets';
 import Loading from '../../components/Loading';
 import Title from '../../components/admin/Title.jsx';
+import { dateFormat } from '../../lib/dateFormat.js';
 import BlurCircle from '../../components/BlurCircle.jsx';
 
 const Dashboard = () => {
@@ -54,6 +55,27 @@ const Dashboard = () => {
           ))}
         </div>
       </div>
+
+     <p className="mt-10 text-lg font-medium">Active Shows</p>
+     <div className="relative grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-4 max-w-6xl x-10">
+  <BlurCircle top="100px" left="-10%" className="absolute z-0"/>
+  {dashboardData.activeShows.map((show) => (
+    <div key={show._id} className="w-full rounded-lg overflow-hidden h-full pb-3 bg-primary/10 border border-primary/20 hover:-translate-y-1 transition duration-300">
+      <img src={show.movie.poster_path} alt='' className="h-60 w-full object-cover" />
+      <p className="font-medium p-2 truncate">{show.movie.title}</p>
+      <div className="flex items-center justify-between px-2">
+        <p className="text-lg font-medium">{currency}{show.showPrice}</p>
+        <p className="flex items-center gap-1 text-sm 
+          text-gray-400 mt-1 pr-1">
+          <StarIcon className="w-4 h-4 text-primary fill-primary" />
+          {show.movie.vote_average.toFixed(1)}
+        </p>
+      </div>
+      <p className='px-2 pt-2 text-sm text-gray-500'>{dateFormat(show.showDateTime)}</p>
+    </div>
+  ))}
+</div>
+
     </>
   ) : <Loading />;
 };
