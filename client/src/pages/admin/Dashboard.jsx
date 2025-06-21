1a09de2de0e80acae2fc3460513f08b715e50289
Sneha Kushwaha <snehakushwaha7394@gmail.com>
2025-06-21 12:06:27 +0530

    added cards in dashboard

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,29 +1,32 @@
-import { ChartLineIcon, CircleDollarSignIcon, PlayCircleIcon, UserIcon } from 'lucide-react';
+import { ChartLineIcon, CircleDollarSignIcon, PlayCircleIcon, UsersIcon } from 'lucide-react';
 import React, { useEffect, useState } from 'react'
 import { dummyDashboardData } from '../../assets/assets';
 import Loading from '../../components/Loading';
+import Title from '../../components/admin/Title.jsx';
+import BlurCircle from '../../components/BlurCircle.jsx';
 
 const Dashboard = () => {
-
-  const currency = import.meta.env.VITE_CURRENCY
+  const currency = import.meta.env.VITE_CURRENCY;
 
   const [dashboardData, setDashboardData] = useState({
-    totalBooking: 0,
+    totalBookings: 0,
     totalRevenue: 0,
-    activeUser: 0
+    activeUser: 0,
+    activeShows: [],       
+    totalUser: 0  
   });
   const [loading, setLoading] = useState(true);
 
   const dashboardCards = [
-    {title: "Total Booking", value: dashboardData.totalBooking || "0", icon:ChartLineIcon },
-    {title: "Total Revenue", value: dashboardData.totalRevenue || "0", icon:CircleDollarSignIcon },
-    {title: "Active Shows", value: dashboardData.activeShows.length || "0", icon:PlayCircleIcon },
-    {title: "Total Users", value: dashboardData.totalUser || "0", icon:UserIcon }
-  ]
+    {title: "Total Bookings", value: dashboardData.totalBookings || "0", icon: ChartLineIcon },
+    {title: "Total Revenue", value: dashboardData.totalRevenue || "0", icon: CircleDollarSignIcon },
+    {title: "Active Shows", value: dashboardData.activeShows.length || "0", icon: PlayCircleIcon },
+    {title: "Total User", value: dashboardData.totalUser || "0", icon: UsersIcon }
+  ];
 
   const fetchDashboardData = async () => {
-    setDashboardData(dummyDashboardData)
-    setLoading(false)
+    setDashboardData(dummyDashboardData);
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -31,10 +34,28 @@ const Dashboard = () => {
   }, []);
 
   return !loading ? (
-    <div>
-        .
-    </div>
-  ) : <Loading />
-}
+    <>
+      <Title text1="Admin" text2="Dashboard" />
 
-export default Dashboard
+      <div className="relative flex flex-wrap gap-4 mt-6">
+        <BlurCircle top="-100px" left="0" />
+        <div className='flex flex-wrap gap-4 w-full'>
+          {dashboardCards.map((card, index)=>(
+            <div key={index} className='flex items-center justify-between px-4 py-3 bg-primary/10 border border-primary/20 rounded-md w-[220px] text-white'> 
+            <div>
+              <h1 className='text-sm'>{card.title}</h1>
+             <p className='text-2xl font-bold mt-1'>
+              {card.title === "Total Revenue" ? currency : ''}
+              {card.value}
+              </p>
+            </div>
+            <card.icon className='w-6 h-6 text-white'/>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  ) : <Loading />;
+};
+
+export default Dashboard;
--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import AdminNavbar from '../../components/admin/AdminNavbar'
-import  AdminSidebar  from '../../components/admin/AdminSidebar'
+import AdminSidebar from '../../components/admin/AdminSidebar'
 import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
     <>
-    <AdminNavbar />
-    <div>
+      <AdminNavbar />
+      <div className="flex min-h-screen">
         <AdminSidebar />
-        <div className='flex-1 px-4 py-10 md:px-10 h-[calc(100vh-64px)] overflow-y-auto'>
-            <Outlet />
+        <div className='flex-1 px-4 py-10 md:px-10 h-[calc(100vh-64px)] overflow-y-auto text-white bg-black'>
+          <Outlet />
         </div>
-    </div>
+      </div>
     </>
   )
 }
